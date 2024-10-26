221b4563c885b33ebf1cff7465810a74457866a4
mier002 <micha.erci@students.fhnw.ch>
2024-10-26 12:00:16 +0200

    Add MUI components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import React from 'react'
+import {Button} from '@mui/material/Button';
+
 function App() {
-  return <h1>Hello React!</h1>;
+  return(
+    <h1 style={{ textAlign: 'center', paddingTop: '10px' }}>
+      <Typography variant="h2" component="h1" gutterBottom>
+        Hello React!
+      </Typography>
+        MUI-Vorlage
+      <Button variant="contained" color="primary">
+        Vorlage
+      </Button>
+    </h1>
+  );
 }
 
 export default App;
